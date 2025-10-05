3ca439fcaec0bd1ad6a62827101a7a5e426f4d40
GeandroMarques <marquess.2608@gmail.com>
2025-10-05 10:36:18 -0300

    Função busca da árvore trie

--- src/ArvoreTrie.java
+++ src/ArvoreTrie.java
@@ -5,5 +5,22 @@ public class ArvoreTrie {
 
     }
 
-
+    public void busca(String palavra) {
+        No atual = raiz;
+        for (int i = 0; i < palavra.length(); i++) {
+            char letraAtual = palavra.charAt(i);
+            No filho = atual.buscar(letraAtual);
+            if (filho != null) {
+                atual = filho;
+            } else {
+                System.out.println("A palavra não existe!");
+                return;
+            }
+        }
+        if (atual.eUltimaLetra()) {
+            System.out.println("A palavra está na árvore!");
+        } else {
+            System.out.println("A palavra não está na árvore!");
+        }
+    }
 }
