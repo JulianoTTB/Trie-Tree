dbbecf84fa9a3e1851d238eb7d87e239d4866b87
Juliano Teles Tolomini Biasotto <julianobiasotto@gmail.com>
2025-10-05 08:51:28 -0300

    Mudando de lista encadeada para array

--- src/No.java
+++ src/No.java
@@ -1,24 +1,66 @@
 public class No {
     private char letra;
     private final No[] filhos;
-    private boolean temFilhos;
+    private int tamanho;
+    private boolean ultimoLetra;
 
-    public No(char letra){
+    public No(char letra, boolean ultimoLetra){
         this.letra = letra;
         this.filhos = new No[37];
-        this.temFilhos = false;
+        this.tamanho = 0;
+        this.ultimoLetra = ultimoLetra;
+    }
+
+    public int getTamanho(){
+        return this.tamanho;
     }
 
     public boolean semFilhos(){
-        return !this.temFilhos;
+        return this.tamanho == 0;
+    }
+
+    public boolean eUltimoLetra(){
+        return this.ultimoLetra;
     }
 
     public char getLetra() {
-        return letra;
+        return this.letra;
     }
 
     public void inserirFilho(No no){
+       if(this.getTamanho() == 37 || this.buscar(no.getLetra()) != null) return;
 
+       for(int i = 0; i < this.tamanho + 1; i++){
+           if (this.filhos[i] == null) {
+               this.filhos[i] = no;
+               break;
+           }
+       }
+       this.tamanho++;
+    }
+
+    public void remover(char letra){
+        if(buscar(letra) == null || semFilhos()) return;
+
+        for(int i = 0; i < this.tamanho; i++){
+            No atual = this.filhos[i];
+            if (atual == null) continue;
+            if (atual.getLetra() == letra) {
+                this.filhos[i] = this.filhos[this.tamanho - 1];
+                this.filhos[tamanho - 1] = null;
+                break;
+            }
+        }
+        this.tamanho--;
+    }
+
+    public No buscar(char letra){
+        for(int i = 0; i < this.tamanho; i++){
+            No atual = this.filhos[i];
+            if (atual == null) continue;
+            if (atual.getLetra() == letra) return atual;
+        }
+        return null;
     }
 
     public No[] getFilhos() {
